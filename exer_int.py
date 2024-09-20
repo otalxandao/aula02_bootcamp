a9edeaae97cce2d6a8f2de89fc4df02c639ca4cc
Alexandre <xandelourenso@gmail.com>
2024-09-20 09:29:46 -0300

    Arrumei o processo para todos as funções rodarem junto com o código

--- exer_int.py
+++ exer_int.py
@@ -2,31 +2,31 @@
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
-#valor_1 = int(input("Coloquei o primeiro valor: "))
-#valor_2 = int(input("Coloquei o segundo valor: "))
-#resultado = print(valor_1 + valor_2)
+valor_1 = int(input("Coloquei o primeiro valor: "))
+valor_2 = int(input("Coloquei o segundo valor: "))
+resultado = print(valor_1 + valor_2)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-#valor_1 = int(input("Coloquei um numero: "))
-#resto = print(valor_1%5)
+valor_1 = int(input("Coloquei um numero: "))
+resto = print(valor_1%5)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
-#valor_1 = int(input("Coloquei o primeiro valor: "))
-#valor_2 = int(input("Coloquei o segundo valor: "))
-#multiplicacao = print(valor_1*valor_2)
+valor_1 = int(input("Coloquei o primeiro valor: "))
+valor_2 = int(input("Coloquei o segundo valor: "))
+multiplicacao = print(valor_1*valor_2)
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 valor_1 = int(input("Coloquei o primeiro valor: "))
 valor_2 = int(input("Coloquei o segundo valor: "))
-divisao_inteira = print(round(valor_1//valor_2))
+divisao_inteira = print(round(valor_1//valor_2))    
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-#valor_quadrado = int(input("Coloquei um numero: "))
-#quadrado = (valor_quadrado*valor_quadrado)
-#resultado_quadrado = print(f'O valor ao quadrado de {valor_quadrado} é {quadrado}')
+valor_quadrado = int(input("Coloquei um numero: "))
+quadrado = (valor_quadrado*valor_quadrado)
+resultado_quadrado = print(f'O valor ao quadrado de {valor_quadrado} é {quadrado}')
 
 
