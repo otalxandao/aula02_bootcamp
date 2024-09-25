58ce66c2185ee0a5724fed3a01332815475f09a5
Alexandre <xandelourenso@gmail.com>
2024-09-25 14:15:30 -0300

    Exer de string mais os ajustes de ambiente

---
+++ string_exer.py
@@ -0,0 +1,20 @@
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+#user_string = input("Colocar uma palavra: ")
+#user_string_maiuscula = user_string.upper()
+
+#print(f'A palavra em maiúcula é: {user_string_maiuscula}')
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+user_string = input("Colocar seu nome: ")
+user_string_maiuscula = user_string.lower()
+
+print(f'A palavra em maiúcula é: {user_string_maiuscula}')
+
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+
