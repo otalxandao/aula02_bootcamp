f5bd3ed94a37f64713331ac46e39ba60648a80a2
Alexandre <xandelourenso@gmail.com>
2024-11-01 08:39:37 -0300

    Inclui os exers para strings

--- string_exer.py
+++ string_exer.py
@@ -10,11 +10,26 @@
 user_string = input("Colocar seu nome: ")
 user_string_maiuscula = user_string.lower()
 
-print(f'A palavra em maiúcula é: {user_string_maiuscula}')
+print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
 
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+user_string = input("Colocar seu nome: ")
+user_string_maiuscula = user_string.lower()
+
+print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
+
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+
+user_string = input("Colocar seu nome: ")
+user_string_maiuscula = user_string.lower()
+
+print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
+
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 
