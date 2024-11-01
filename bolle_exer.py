1fd6f24270f1e6125ca8ae7d7d7cdedfa8824de7
Alexandre <xandelourenso@gmail.com>
2024-11-01 09:23:09 -0300

    iniciei e terminei os exers de booleanos

---
+++ bolle_exer.py
@@ -0,0 +1,37 @@
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# Exemplo de entrada
+valor1 = True
+valor2 = False
+resultado_and = valor1 and valor2
+print("Resultado do AND lógico:", resultado_and)
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+valor1 = input("Coloque o seu booleano aqui: ")
+valor2 = input("Coloque o seu booleano aqui: ")
+resultado_and = valor1 or valor2
+print("Resultado do AND lógico:", resultado_and)
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+valor1 = input("Coloque o seu booleano aqui: ")
+resultado_and = not valor1 
+print("Resultado do AND lógico:", resultado_and)
+
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+
+valor1 = float(input("Coloque o seu booleano aqui: "))
+valor2 = float(input("Coloque o seu booleano aqui: "))
+resultado_and = (valor1 == valor2)
+print("Resultado para serem iguais:", resultado_and)
+
+
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.a
+
+
+valor1 = float(input("Coloque o seu booleano aqui: "))
+valor2 = float(input("Coloque o seu booleano aqui: "))
+resultado_and = (valor1 != valor2)
+print("Resultado para serem iguais:", resultado_and)
