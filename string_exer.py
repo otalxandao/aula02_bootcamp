5428b6ab77dfb1d0efdda8297e6b1a4d2c2ad918
Alexandre <xandelourenso@gmail.com>
2024-11-01 09:22:31 -0300

    Terminei os exers de string

--- string_exer.py
+++ string_exer.py
@@ -7,29 +7,35 @@
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-user_string = input("Colocar seu nome: ")
-user_string_maiuscula = user_string.lower()
+#user_string = input("Colocar seu nome: ")
+#user_string_maiuscula = user_string.lower()
 
-print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
+#print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
 
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 
-user_string = input("Colocar seu nome: ")
-user_string_maiuscula = user_string.lower()
+##user_string = input("Colocar seu nome: ")
+##user_string_maiuscula = user_string.lower()
 
-print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
+##print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
 
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
 
-user_string = input("Colocar seu nome: ")
-user_string_maiuscula = user_string.lower()
+#user_string = input("Colocar seu nome: ")
+#user_string_maiuscula = user_string.lower()
 
-print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
+#print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
 
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 
+## Colocar strip para evitar espaços em branco
+user_string_1 = input("Colocar seu nome: ").strip()
+user_string_2 = input("Colocar seu nome: ").strip()
+user_string_maiuscula = user_string_1+" "+user_string_2
+
+print(f'O nome com todas as letras minúsculas é: {user_string_maiuscula}')
